2afa23f78f79bd333102b13aa9e510a6cd4b9b57
Puneetokok <puneet30ok@gmail.com>
2025-02-20 11:29:15 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,10 @@
+
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(my_list[2:6])   
+print(my_list[:5])    
+print(my_list[5:])    
+print(my_list[::2])   
+print(my_list[::-1])  
+print(my_list[-5:-2]) 
+print(my_list[1:8:2])
+
